2af4f819ee79777fce16b0a16e0b077969d37942
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-07-04 12:12:48 +0300

    check

--- Landmarks/Views/Profile/ProfileSummary.swift
+++ Landmarks/Views/Profile/ProfileSummary.swift
@@ -27,7 +27,7 @@ struct ProfileSummary: View {
                 HStack {
                     HikeBadge(name: "First Hike")
                     HikeBadge(name: "Earth Day")
-                        .hueRotation(Angle(degrees: 95))
+                        .hueRotation(Angle(degrees: 90))
                     HikeBadge(name: "Tenth Hike")
                         .grayscale(0.5)
                         .hueRotation(Angle(degrees: 45))
